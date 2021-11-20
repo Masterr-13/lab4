a70637b8012d03394aa65c145963fd83f3367c24
User1 <User1@MSI-GE70>
2021-11-20 17:04:34 +0300

    Добавьте файлы проекта.

---
+++ Задача 4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Задача_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число больше 0: ");
+            int n = Convert.ToInt32 (Console.ReadLine());
+            int a;
+            int sum = 0;
+           
+            if (n<=0)
+                Console.WriteLine("Ошибка!");
+
+            for (int i = 1; i <= n; i++)
+            {
+                a = 2 * i - 1;
+                sum += a;
+                Console.WriteLine(sum);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
